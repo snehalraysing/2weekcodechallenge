5bb0d31b679abe10a28725f6489651f5e57ec87b
snehalraysing <snehalraysing9890@gmail.com>
2022-07-11 22:32:56 +0530

    day3

---
+++ day3/pattern.py
@@ -0,0 +1,16 @@
+#333333
+#33333
+#3333
+#333
+#33
+#3
+
+def func():
+    n = 6
+    for i in range(n,0,-1):
+        for j in range(0,i):
+            print("3", end=" ")
+        print()
+
+func()
+
--- day3/pattern5.py
+++ day3/pattern5.py
@@ -13,10 +13,16 @@ def func():
     c = 9
     for i in range(0,r):
         for j in range(0,c):
-            if(i==r-1 and j==c-4):
+            if((i==r-1 and j==c-4) or (i==r-2 and j==c-5) or
+                    (i==r-2 and j==c-3) or (i==r-3 and j==c-6) or (i==r-3 and j==c-6) or
+                    (i==r-3 and j==c-2) or (i==r-4 and j==c-7) or (i==r-4 and j==c-1) or
+                    (i==r-5 and j==c-8) or(i==r-5 and j==c-1) or (i==r-6 and j==c-7) or
+                    (i==r-6 and j==c-6) or (i==r-6 and j==c-5) or (i==r-5 and j==c-4) or
+                    (i==r-6 and j==c-3) or (i==r-6 and  j==c-2) or (i==r-6 and j==c-1)):
                 print("*" , end=" ")
             else:
                 print(" ", end=" ")
         print()
 
 func()
+
