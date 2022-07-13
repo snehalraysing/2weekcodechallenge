91812e5bc2f5b64f2d689d0c91b20f236c2d31b6
snehalraysing <snehalraysing9890@gmail.com>
2022-07-13 10:50:45 +0530

    day5

--- day5/asciivalue.py
+++ day5/asciivalue.py
@@ -1,1 +1,2 @@
 #Write a Program to Find ASCII Value of a Character
+
--- day5/swap2no.py
+++ day5/swap2no.py
@@ -4,7 +4,13 @@ n1 = int(input("Enter a number: "))
 n2 = int(input("Enter another number: "))
 print("Numbers before swapping: ", n1," ", n2)
 
+#4 <--- #4
+temp = n1
 
+#3 <--#3
+n1=n2
 
+#4 <---#4
+n2 = temp
 
 print("Numbers after swapping: ",n1," ",n2)
