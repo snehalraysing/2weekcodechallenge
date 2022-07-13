fe5ec37a81f7e8cc7d5eb74fc9048f9a5b6d1fce
snehalraysing <snehalraysing9890@gmail.com>
2022-07-13 11:01:38 +0530

    day5

--- day5/asciivalue.py
+++ day5/asciivalue.py
@@ -1,2 +1,11 @@
 #Write a Program to Find ASCII Value of a Character
 
+#ord() ascii value of character 'S'
+#chr() encodes the character of the ascii value
+
+character = input("Enter Any Character: ")
+print("ASCII value of a character is: ", ord(character))
+
+num = int(input("Enter Any Number: "))
+print("The Character is: ", chr(num))
+
--- day5/multiply2deci.py
+++ day5/multiply2deci.py
@@ -1,2 +1,11 @@
-#Write a Program to Multiply two decimal Numbersentered by User
+#Write a Program to Multiply two decimal Numbers entered by User
 
+def mul():
+    n1 = float(input("Enter first decimal number: "))
+    n2 = float(input("Entre second decimal number: "))
+
+    solution = n1 * n2
+
+    print("Multiplication is: ",solution)
+
+mul()
