a6891428d45f07099531de32e99b2746df0c5528
snehalraysing <snehalraysing9890@gmail.com>
2023-03-09 21:39:56 +0530

    Create question.py

---
+++ question.py
@@ -0,0 +1,16 @@
+#12,24,36,48,60
+
+arr = [12,24,36,48,60]
+#print(len(arr))
+
+def func():
+    i = 0
+    sum1[] = 0
+    sum2[] = 0
+    for i in range(0,len(arr)):
+        #print(arr[i])
+
+
+
+func()
+
